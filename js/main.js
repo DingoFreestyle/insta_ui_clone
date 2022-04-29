3788f6e1fb18fa081be3295cb12637355c74609b
DingoFreestyle <104494672+DingoFreestyle@users.noreply.github.com>
2022-04-29 16:43:31 +0900

    JQUERY 각종 이벤트 요소들 및 이용 방법 + 단축법 학습완료

--- js/main.js
+++ js/main.js
@@ -13,18 +13,50 @@
 
 // document.getElementsByTagName('h1')[0].style.color = 'blue';
 
-$(document).ready(function () {
-    $('h1').css({'color': 'pink'});
-    $('#typo .inner').css({
-        'text-decoration': 'underline',
-        'border-bottom': '5px solid black',
-        'opacity': 0.5,
-        'transform': 'rotate(45deg)'});
-})
+// $(document).ready(function () {
+//     $('h1').css({'color': 'pink'});
+//     $('#typo .inner').css({
+//         'text-decoration': 'underline',
+//         'border-bottom': '5px solid black',
+//         'opacity': 0.5,
+//         'transform': 'rotate(45deg)'});
+// })
 
 // CSS 스타일 적용 >>>>>> """ $(html요소).메소드css나 js같은거('매개변수1','매개변수2'); """
 
 
+////////////////// 마우스 반응 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
 
+$(document).ready(function () {
+    $('#logo').on('mouseover', function () {   ///////  ".on"을 사용해서 작동! 을 표헌 / - mouseover > 커서가 올라왔을 때,
+        $('#logo').css({'transform': 'scale(0.8)'});
+    $('#logo').on('mouseout', function () {   ///////  - mouseout > 커서가 나왔을 때,
+        $('#logo').css({'transform': 'scale(1)'});
+        });
+    });
+})
+///////////////////////////////////////////////////
+///// 윗 내용을 아래처럼 줄일 수도 있으니 참고///////////
 
+// $(document).ready(function () {
+//     $('#logo').mouseover(function () {
+//         $('#logo').css({'transform': 'scale(0.8)'});
+//     })
+//     .mouseout(function () {
+//         $('#logo').css({'transform': 'scale(1)'});
+//         });
+// })
 
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+// 이 외 이벤트의 종류는???
+//  keydown - 요소에 초첨이 맞춰진 상태에서 키보드 눌렸을 때
+//  keyup - 요소에 초첨이 맞춰진 상태에서 키보드 뗄 때
+//  focus - 요소에 포커스가 맞았을 때 (포커스 있는 요소 한정)
+//  blur - 요소에 포커스에서 떨어졌을 때 (포커스 있는 요소 한정)
+//  change - 입력 내용 변경시 (textarea, input, select 등등)
+//  resize - 크기 변경시
+//  scroll - 스크롤 시
+
+///////////////////////////////////////////////////
